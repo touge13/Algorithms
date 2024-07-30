86c84d757bbaeff29f9d7b12eeeb14c95ad8acb3
Mikhail Grudinin <76101411+touge13@users.noreply.github.com>
2024-07-30 19:46:42 +0300

    Create 1450. Number of Students Doing Homework at a Given Time.py

---
+++ yandex/1450. Number of Students Doing Homework at a Given Time.py
@@ -0,0 +1,7 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        res = 0
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime and endTime[i] >= queryTime:
+                res += 1
+        return res
